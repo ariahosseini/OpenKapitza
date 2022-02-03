b97f369415e310538c1d7329c7f43bb1177b38dd
Aria Hosseini <aria_h@mit.edu>
2022-02-03 11:05:05 -0800

    Add plot functions

--- OpenKapitza/functions.py
+++ OpenKapitza/functions.py
@@ -1,5 +1,10 @@
 """Provide the primary functions."""
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+sns.set_style("white", {"xtick.major.size": 2, "ytick.major.size": 2})
+sns.set_context("paper", font_scale=2, rc={"lines.linewidth": 4})
 
 
 def read_hessian(file_name: str) -> np.ndarray:
@@ -25,27 +30,58 @@ def read_hessian(file_name: str) -> np.ndarray:
     return hessian
 
 
-def plot_2Darray(array_to_plot) -> None:
+def plot_2Darray(array_to_plot: np.ndarray, set_title: str, x_label: str = None, y_label: str = None) -> None:
     """
-    A function to plot two dimennsionnal arrays
+    A function to plot two-dimensional arrays
 
     Parameters
     ----------
-    file_name : str
-        Lammps output file -- hessian-mass-weighted-hessian.d
+    array_to_plot : np.ndarray
+        Numpy two-dimensional  arrays we are plotting
+    set_title : str
+        Title of the plot
+    x_label : str
+        Heatmap x label
+    y_label : str
+        Heatmap y label
 
     Returns
     -------
-    hessian : np.ndarray
-        Phonon hessian matrix -- symmetric
+    None
     """
 
-    hessian_data_file = np.loadtxt(file_name, delimiter=None, skiprows=0)
-    hessian_symmetric = (np.triu(hessian_data_file, k=0) + np.tril(hessian_data_file, k=0).T) / 2
-    hessian = np.triu(hessian_symmetric) + np.triu(hessian_symmetric, k=1).T
+    plt.figure(figsize=(6.5, 6.5))
+    ax = sns.heatmap(array_to_plot, linewidth=0.1, cbar=False)
+    ax.set_frame_on(False)
+    ax.tick_params(axis='y', labelleft='off')
+    ax.set_xlabel(x_label, fontsize=24)
+    ax.set_ylabel(y_label, fontsize=24, labelpad=15)
+    ax.tick_params(axis="y", labelsize=24)
+    ax.tick_params(axis="x", labelsize=24)
+    ax.set_title(set_title)
+    ax.set(xticklabels=[])
+    ax.set(yticklabels=[])
+    plt.tight_layout()
+    plt.show()
 
-    return hessian
+def read_crystal(file_name: str, skiprows: int = 9) -> np.ndarray:
+    """
+    Placeholder function to show example docstring (NumPy format).
 
+    Replace this function and doc string for your own project.
+
+    Parameters
+    ----------
+    with_attribution : bool, Optional, default: True
+        Set whether or not to display who the quote is from.
+
+    Returns
+    -------
+    quote : str
+        Compiled string including quote and optional attribution.
+    """
+
+    crystal_points = np.loadtxt(file_name, delimiter=None, skiprows=skip_rows)
 
 def canvas(with_attribution=True):
     """
