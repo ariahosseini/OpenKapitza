50d311fe53afa4f9725e446d1817a5af1cbbb0de
Aria Hosseini <aria_h@mit.edu>
2022-02-04 00:03:47 -0800

    Continue adding functions

--- OpenKapitza/functions.py
+++ OpenKapitza/functions.py
@@ -28,7 +28,8 @@ def read_hessian(file_name: str) -> np.ndarray:
     return hessian
 
 
-def plot_2darray(array_to_plot: np.ndarray, pic_name: str, set_title: str, x_label: str = None, y_label: str = None) -> None:
+def plot_2darray(array_to_plot: np.ndarray, pic_name: str, set_title: str, x_label: str = None,
+                 y_label: str = None) -> None:
     """
     A function to plot two-dimensional arrays
 
@@ -92,7 +93,6 @@ def read_crystal(file_name: str, natm_per_unitcell: int, skip_rows: int = 9) -> 
 
 def mitigate_periodic_effect(
         file_hessian: str, file_crystal: str, natm_per_unitcell: int, rep: list, skip_rows: int = 9) -> dict:
-
     hessian = read_hessian(file_hessian)
 
     with open(file_crystal, 'r') as read_obj:
@@ -106,22 +106,58 @@ def mitigate_periodic_effect(
                 break
         lattice_constant: np.array = \
             np.array([float(x_max) - float(x_min), float(y_max) - float(y_min), float(z_max) - float(z_min)]) / \
-            np.array([rep[0], rep[1], 2*rep[2]])
+            np.array([rep[0], rep[1], 2 * rep[2]])
     crystal_info = read_crystal(file_crystal, natm_per_unitcell, skip_rows)
     lattice_points = crystal_info['lattice_points']
-    lattice_points[::2*rep[2]] = np.inf
-    lattice_points[2*rep[2]-1::2*rep[2]] = np.inf
+    lattice_points[::2 * rep[2]] = np.inf
+    lattice_points[2 * rep[2] - 1::2 * rep[2]] = np.inf
     lp = lattice_points[np.isfinite(lattice_points).all(1)]
 
-    for io in range(natm_per_unitcell*3):
-        hessian[io::natm_per_unitcell*3*rep[2]*2] = np.inf
+    for io in range(natm_per_unitcell * 3):
+        hessian[io::natm_per_unitcell * 3 * rep[2] * 2] = np.inf
         hessian[:, io::natm_per_unitcell * 3 * rep[2] * 2] = np.inf
-        hessian[io + natm_per_unitcell*3 * (rep[2] * 2 - 1)::natm_per_unitcell*3*rep[2]*2] = np.inf
+        hessian[io + natm_per_unitcell * 3 * (rep[2] * 2 - 1)::natm_per_unitcell * 3 * rep[2] * 2] = np.inf
         hessian[:, io + natm_per_unitcell * 3 * (rep[2] * 2 - 1)::natm_per_unitcell * 3 * rep[2] * 2] = np.inf
     hsn = hessian[~(np.isinf(hessian).all(axis=1))]
     hsn_matrix = np.transpose(hsn.T[~(np.isinf(hsn.T).all(axis=1))])
 
-    return {'hsn_matrix': hsn_matrix, 'lattice_points':lp}
+    return {'hsn_matrix': hsn_matrix, 'lattice_points': lp, 'lattice_constant': lattice_constant}
+
+
+def matrix_decomposition(hsn_matrix: np.darray, indices: list,
+                         block_size: int, rep: list, natm_per_unitcell: int) -> dict:
+    """
+    A function to read unwrapped atoms position from lammps output and compute lattice points
+
+    Parameters
+    ----------
+    file_name: str
+        Lammps output file — data.wrapper
+    natm_per_unitcell : int
+        Number of atoms per unit cell
+    skip_rows: int
+        Number of lines to skip in data.unwrapped
+
+    Returns
+    -------
+    output : dict
+        First key includes the crystal points and the second key includes the lattice points
+    """
+
+    f_idx = np.array([[0, 0, 0], [0, -1, 0], [0, 1, 0], [0, 0, -1], [0, 0, 1],
+                    [1, -1, 0], [1, 1, 0], [1, 0, 0], [1, 0, -1]])
+    elements_idx = (indices[2] - 1) + ((indices[1]+f_idx[:, 0] - 1) * rep[0] + indices[0] + f_idx[:, 1]- 1) * 2 * rep[2]
+
+    Hsn_keys = ['H0', 'H1', 'H2', 'H3', 'H4', 'T1', 'T2', 'T3', 'T4']
+    Hsn = {}
+    for i in range(7):
+        Hsn_block = hsn_matrix[natm_per_unitcell * 3 * elements_idx[i]: natm_per_unitcell * 3 * (elements_idx[i] + block_size),
+         natm_per_unitcell * 3 * elements_idx[0]: natm_per_unitcell * 3 * (elements_idx[0] + block_size)]
+        Hsn[Hsn_keys[i]] = Hsn_block
+
+
+
+    return Hsn
 
 
 if __name__ == "__main__":
