e841eb1470db58d567265ed118f2fd019dbd194c
Aria Hosseini <aria_h@mit.edu>
2022-02-04 18:21:06 -0800

    Add an efficient map function to compute hessian_fourier_form

--- OpenKapitza/functions.py
+++ OpenKapitza/functions.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from numpy import ndarray
+import functools
 
 sns.set()
 sns.set_style("white", {"xtick.major.size": 2, "ytick.major.size": 2})
@@ -92,9 +92,7 @@ def read_crystal(file_name: str, natm_per_unitcell: int, skip_rows: int = 9) -> 
 
 
 def mitigate_periodic_effect(
-        file_hessian: str, file_crystal: str, natm_per_unitcell: int, rep: list, skip_rows: int = 9) -> dict:
-    hessian = read_hessian(file_hessian)
-
+        hessian: np.ndarray, crystal_info: dict, file_crystal: str, natm_per_unitcell: int, rep: list, skip_rows: int = 9) -> dict:
     with open(file_crystal, 'r') as read_obj:
         # Read all lines in the file one by one
         for line_number, line in enumerate(read_obj):
@@ -107,7 +105,6 @@ def mitigate_periodic_effect(
         lattice_constant: np.array = \
             np.array([float(x_max) - float(x_min), float(y_max) - float(y_min), float(z_max) - float(z_min)]) / \
             np.array([rep[0], rep[1], 2 * rep[2]])
-    crystal_info = read_crystal(file_crystal, natm_per_unitcell, skip_rows)
     lattice_points = crystal_info['lattice_points']
     lattice_points[::2 * rep[2]] = np.inf
     lattice_points[2 * rep[2] - 1::2 * rep[2]] = np.inf
@@ -124,7 +121,7 @@ def mitigate_periodic_effect(
     return {'hsn_matrix': hsn_matrix, 'lattice_points': lp, 'lattice_constant': lattice_constant}
 
 
-def matrix_decomposition(hsn_matrix: np.darray, indices: list,
+def matrix_decomposition(hsn_matrix: np.ndarray, indices: list,
                          block_size: int, rep: list, natm_per_unitcell: int) -> dict:
     """
     A function to read unwrapped atoms position from lammps output and compute lattice points
@@ -144,13 +141,14 @@ def matrix_decomposition(hsn_matrix: np.darray, indices: list,
         First key includes the crystal points and the second key includes the lattice points
     """
 
-    f_idx = np.array([[0, 0, 0], [0, -1, 0], [0, 1, 0], [0, 0, -1], [0, 0, 1],
-                    [1, -1, 0], [1, 1, 0], [1, 0, 0], [1, 0, -1]])
-    elements_idx = (indices[2] - 1) + ((indices[1]+f_idx[:, 0] - 1) * rep[0] + indices[0] + f_idx[:, 1]- 1) * 2 * rep[2]
+    f_idx = np.array([[0, 0, 0], [0, -1, 0], [0, 1, 0], [-1, 0, 0], [1, 0, 0],
+                    [0, -1, 1], [0, 1, 1], [-1, 0, 1], [1, 0, 1]])
+    elements_idx = (indices[2] + f_idx[:, 2] - 1) + ((indices[1]+f_idx[:, 1] - 1)
+                                                     * rep[0] + indices[0] + f_idx[:, 0] - 1) * 2 * rep[2]
 
     Hsn_keys = ['H0', 'H1', 'H2', 'H3', 'H4', 'T1', 'T2', 'T3', 'T4']
     Hsn = {}
-    for i in range(7):
+    for i in range(9):
         Hsn_block = hsn_matrix[natm_per_unitcell * 3 * elements_idx[i]: natm_per_unitcell * 3 * (elements_idx[i] + block_size),
          natm_per_unitcell * 3 * elements_idx[0]: natm_per_unitcell * 3 * (elements_idx[0] + block_size)]
         Hsn[Hsn_keys[i]] = Hsn_block
@@ -207,21 +205,52 @@ def hessian_fourier_form(Hsn: dict, periodicity_lenght: float, wavevector: np.nd
         First key includes the crystal points and the second key includes the lattice points
     """
 
-    distance_vector = periodicity_lenght*[[0, 0], [0, -1], [0, 1], [-1, 0], [1, 0]].T
-    unit_planewave = np.exp(-1j*(wavevector*distance_vector))
 
-    Hsn_keys = np.arange(np.shape(wavevector, 1))
+
+    distance_vector = periodicity_lenght*np.array([[0, 0], [0, -1], [0, 1], [-1, 0], [1, 0]])
+    unit_planewave = np.exp(-1j*(np.matmul(distance_vector, wavevector)).T)
+    print(Hsn.keys())
+    print(np.shape(unit_planewave))
+    Hsn_keys = np.arange(np.shape(wavevector)[1])
     Hsn_fourier = {}
     Hopping_fourier = {}
-    for i in range(len(Hsn_keys)):
-        Hsn_fourier[Hsn_keys[i]] = Hsn['H0']*unit_planewave(i, 0) + Hsn['H1']*unit_planewave(i, 1)
-        + Hsn['H2']*unit_planewave(i, 2) + Hsn['H3']*unit_planewave(i, 3) + Hsn['H4']*unit_planewave(i, 4)
 
-        Hopping_fourier[Hsn_keys[i]] = Hsn['T1']*unit_planewave(i, 1) + Hsn['T2']*unit_planewave(i, 2) + \
-                                       Hsn['T3']*unit_planewave(i, 3) + Hsn['T4']*unit_planewave(i, 4)
-    Hsn_matrix_fourier = {Hsn_fourier, Hopping_fourier}
+    for i in range(len(Hsn_keys)):
+
+        Hsn_fourier[Hsn_keys[i]] = Hsn['H0']*unit_planewave[i, 0] + Hsn['H1']*unit_planewave[i, 1]
+        + Hsn['H2']*unit_planewave[i, 2] + Hsn['H3']*unit_planewave[i, 3] + Hsn['H4']*unit_planewave[i, 4]
+
+        Hopping_fourier[Hsn_keys[i]] = Hsn['T1']*unit_planewave[i, 1] + Hsn['T2']*unit_planewave[i, 2] + \
+                                       Hsn['T3']*unit_planewave[i, 3] + Hsn['T4']*unit_planewave[i, 4]
+    Hsn_matrix_fourier = {'Hsn_fourier': Hsn_fourier, 'Hopping_fourier': Hopping_fourier}
 
     return Hsn_matrix_fourier
+
+
+def hessian_fourier_form_two(Hsn: dict, periodicity_lenght: float, wavevector: np.ndarray) -> np.ndarray:
+
+    distance_vector = periodicity_lenght*np.array([[0, 0], [0, -1], [0, 1], [-1, 0], [1, 0]])
+    unit_planewave = np.exp(-1j*(np.matmul(distance_vector, wavevector)).T)
+    Hsn_fourier = {}
+    Hopping_fourier = {}
+
+    def fourier_transform(Hsn_mat, planewave):
+        Hsn_fourier = Hsn_mat['H0'] * planewave[0] + Hsn_mat['H1'] * planewave[1]\
+                                   + Hsn_mat['H2'] * planewave[2] + Hsn_mat['H3'] * planewave[3]\
+                                   + Hsn_mat['H4'] * planewave[4]
+
+        Hopping_fourier = Hsn_mat['T1'] * planewave[1] + Hsn_mat['T2'] * planewave[2] + \
+                                       Hsn_mat['T3'] * planewave[3] + Hsn_mat['T4'] * planewave[4]
+        Hsn_matrix = {'Hsn_fourier': Hsn_fourier, 'Hopping_fourier': Hopping_fourier}
+
+        return Hsn_matrix
+
+    f_transform = functools.partial(fourier_transform, Hsn)
+
+    Hsn_matrix_fourier = map(f_transform, unit_planewave)
+
+    return Hsn_matrix_fourier
+
 
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
